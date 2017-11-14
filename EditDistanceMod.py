c24f0e65046559a86d40d5984a72b6fbd5538653
Unknown <jfenech22@hotmail.com>
2017-11-14 17:52:38 +0100

    Finished
    editdistancefromwiki contains EditDistance module that returns the values (remember it RETURNS)

---
+++ EditDistanceMod.py
@@ -0,0 +1,9 @@
+import scipy as sp
+from editdistancefromwiki import EditDistance as ed
+
+#Trial
+s= 'abdee'
+
+t= 'ade'
+
+ed(s,t)
--- editdistancefromwiki.py
+++ editdistancefromwiki.py
@@ -8,17 +8,17 @@ def EditDistance(s,t): # s,t are our 2 strings
     #the first i characters of s and the first j characters of t.
     #s has dimension m and t has dimension n
     #Note that d has (m+1) x (n+1) values.
-    print len(s)
+    #print len(s)
     m = len(s)
     n = len(t)
-    print m,n
-    d = sp.zeros[(m,n)]
-    for i in range(m):
+    #print m,n
+    d = sp.zeros((m,n))
+    for i in range(0,m):
         d[i,0] = i
     for j in range(n):
         d[0,j] = j
-    for j in range(n):
-        for i in range(m):
+    for j in range(1,n):
+        for i in range(1,m):
             if s[i] == t[j]:
                 d[i,j] = d[i-1,j-1]
             else:
@@ -26,12 +26,9 @@ def EditDistance(s,t): # s,t are our 2 strings
                              (d[i,j-1]+1),
                              (d[i-1,j-1]+1)
                              )
-    return d[m,n]
+    #print d[i,j] #This was to check it works
+    return d[i,j]
 
-#Trial
-s= 'abdee'
 
-t= 'ade'
-EditDistance(s,t)
-print type(s)
-print m,n
+
+
