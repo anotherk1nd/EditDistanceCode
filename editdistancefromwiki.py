325aeb581b3cc97248cc97d9ce12a78e37230511
Unknown <jfenech22@hotmail.com>
2017-11-17 15:33:12 +0100

    Fixing the algo
    Got it to work in scratch but need to update the module itself

--- editdistancefromwiki.py
+++ editdistancefromwiki.py
@@ -13,20 +13,20 @@ def EditDistance(s,t): # s,t are our 2 strings
     n = len(t)
     #print m,n
     d = sp.zeros((m,n))
-    for i in range(0,m):
+    for i in range(m): # range starts from 0
         d[i,0] = i
     for j in range(n):
         d[0,j] = j
-    for j in range(n):
-        for i in range(m):
+    for j in range(1,n):
+        for i in range(1,m):
             if s[i] == t[j]:
-                d[i,j] = d[i-1,j-1]
+                d[i,j] = d[i-1,j-1] # If they are same, get value from subproblems of strings excluding that value
             else:
-                d[i,j] = min((d[i-1,j]+1),
+                d[i,j] = min((d[i-1,j]+1), # If they are different, choose subprob with lowest value, add 1 to indicate operation is necessary
                              (d[i,j-1]+1),
                              (d[i-1,j-1]+1)
                              )
-    #print d[i,j] #This was to check it works
+    print d #This was to check it works
     return int(d[m-1,n-1])
 
 
