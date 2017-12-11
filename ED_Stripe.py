eb7377d4542ac52795e72b6cbbbb5ec2a9f1b923
Unknown <jfenech22@hotmail.com>
2017-12-11 12:04:28 +0100

    some changes
    commented out some code, more results

--- ED_Stripe.py
+++ ED_Stripe.py
@@ -7,15 +7,16 @@ UPDATE: Can initialise empty array, only care about time complexity, not space
 
 import scipy as sp
 
+"""
 def ED_Stripe(s,t,k):
     m = len(s) + 1
     n = len(t) + 1
-    """ Tried to take account of which string is longer
+    Tried to take account of which string is longer
     if m>n: 
         s = s1
         t = t1
         t1 = t
-        s1 = s"""
+        s1 = s
     d = sp.zeros((m,n))
     for i in range(m):  # range starts from 0 (maybe can use range k?
         d[i, 0] = i
@@ -49,7 +50,7 @@ def ED_Stripe(s,t,k):
 s = 'abdee'
 t = 'ade'
 print ED_Stripe(s,t,2)
-
+"""
 
 """
 def ED_Stripe(s,t,k):
@@ -82,3 +83,56 @@ for i in range(len(s)+1):
             print strip
 #print strip
 """
+
+# K STRIP ALGORITHM
+def kstrip(s1, s2, D=2):
+
+    # K value exception
+    if k > min((len(s1)), (len(s2))):
+        print "k smaller than s1 or s2"
+
+    # INITIALIZATION
+    m= len(s)
+    n=len(t)
+    d = sp.zeros((m, n))
+    for i in range(m):  # range starts from 0 (maybe can use range k?
+        d[i, 0] = i
+    for j in range(n):
+        d[0, j] = j
+
+    # Offset counter
+    offset = - (k-2)
+    # Limit counter
+    cap = k + 1 + abs(len(s1) - len(s2))
+    # Loop for K strips around the main diagonal
+    for i in range(1, d.shape[0]):
+        for j in range(max(1, offset), cap):
+            # first condition : i is an insertion
+            print d
+            print i,j
+            con1 = d[i - 1, j] + 1
+
+            # second condition : j is a deletion
+            con2 = d[i, j - 1] + 1
+
+            # third condition : i and j are a substitution
+            if s1[i - 1] == s2[j - 1]:
+                # if same letters, we add nothing
+                con3 = d[i - 1, j - 1]
+            else:
+                # if different letters, we add one
+                con3 = d[i - 1, j - 1] + 1
+
+            # assign minimum value
+            d[i,j] = min(con1, con2, con3)
+            # print("con1: {} con2: {} con3: {} min: {}".format(con1, con2, con3, m[i][i]))
+            # Saving Result
+        #offset += 1
+        #if cap < m.shape[1]:
+            #cap += 1
+    return d
+
+s = 'abdee'
+t = 'ade'
+
+kstrip(s,t,1)
